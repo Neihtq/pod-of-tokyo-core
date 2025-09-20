7499a7a4139ce402f9af2efefe17814539edb063
Quang Thien Nguyen <thienlon@Quangs-MacBook-Air.local>
2025-09-20 20:21:08 +0200

    allow player constructore kwargs

--- pod_of_tokyo_commons/entities/player.py
+++ pod_of_tokyo_commons/entities/player.py
@@ -1,9 +1,17 @@
 class Player:
-    def __init__(self, player_state):
-        self.health = player_state["health"]
-        self.energy = player_state["energy"]
-        self.score = player_state["score"]
-        self.location = player_state["location"]
+    def __init__(
+        self, player_state=None, health=None, energy=None, score=None, location=None
+    ):
+        if player_state:
+            self.health = player_state["health"]
+            self.energy = player_state["energy"]
+            self.score = player_state["score"]
+            self.location = player_state["location"]
+        else:
+            self.health = health
+            self.energy = energy
+            self.score = score
+            self.location = location
 
     def to_dict(self):
         return {
