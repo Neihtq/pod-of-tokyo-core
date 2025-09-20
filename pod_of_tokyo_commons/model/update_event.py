090d65244b5d654a8fc8a531f1d797a67c230ef5
Quang Thien Nguyen <thienlon@Quangs-MacBook-Air.local>
2025-09-21 00:25:04 +0200

    add score field to UpdateEvent

--- pod_of_tokyo_commons/model/update_event.py
+++ pod_of_tokyo_commons/model/update_event.py
@@ -1,14 +1,16 @@
 class UpdateEvent:
-    def __init__(self, location, updates=None, health=0, damage=0, energy=0):
+    def __init__(self, location, updates=None, health=0, damage=0, energy=0, score=0):
         if updates:
             self.health = int(updates["health"])
             self.damage = int(updates["damage"])
             self.energy = int(updates["energy"])
+            self.score = int(updates["score"])
             self.location = updates["location"]
         else:
             self.health = health
             self.damage = damage
             self.energy = energy
+            self.score = score
             self.location = location
 
     def to_dict(self):
@@ -16,5 +18,6 @@ class UpdateEvent:
             "health": self.health,
             "damage": self.damage,
             "energy": self.energy,
+            "score": self.score,
             "location": self.location,
         }
