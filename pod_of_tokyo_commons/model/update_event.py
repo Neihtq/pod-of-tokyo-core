9ac62b01dbc0ad8c09c807bde20a1126cb76d6fe
Quang Thien Nguyen <thienlon@Quangs-MacBook-Air.local>
2025-09-20 23:51:25 +0200

    give update event constructor for dict

--- pod_of_tokyo_commons/model/update_event.py
+++ pod_of_tokyo_commons/model/update_event.py
@@ -1,9 +1,15 @@
 class UpdateEvent:
-    def __init__(self, location, health=0, damage=0, energy=0):
-        self.health = health
-        self.damage = damage
-        self.energy = energy
-        self.location = location
+    def __init__(self, location, updates=None, health=0, damage=0, energy=0):
+        if updates:
+            self.health = updates["health"]
+            self.damage = updates["damage"]
+            self.energy = updates["energy"]
+            self.location = updates["location"]
+        else:
+            self.health = health
+            self.damage = damage
+            self.energy = energy
+            self.location = location
 
     def to_dict(self):
         return {
